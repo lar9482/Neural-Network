e7e48b4a4ce25b46611adc9a9080002356bb819e
Luke Runnels <lar9482@utulsa.edu>
2022-08-07 22:14:28 -0500

    Add project files.

---
+++ Neural Network/MatrixLibrary/Matrix.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Neural_Network.MatrixLibrary
+{
+    public class Matrix
+    {
+        public double[,] data { get; set; }
+        public int rows { get; set; }
+        public int cols { get; set; }
+
+        public Matrix(double[,] data, int rows, int cols)
+        {
+            this.data = data;
+            this.rows = rows;
+            this.cols = cols;
+        }
+
+        public Matrix(int rows, int cols)
+        {
+            this.rows = rows;
+            this.cols = cols;
+
+            this.data = generateRandomArray(rows, cols);
+        }
+
+        public Matrix(double[,] data)
+        {
+            this.data = data;
+            this.rows = data.GetLength(0);
+            this.cols = data.GetLength(1);
+            
+        }
+
+        private double[,] generateRandomArray(int rows, int cols)
+        {
+            double[,] data = new double[rows, cols];
+            Random rand = new Random();
+            for (int cur_Row = 0; cur_Row < rows; cur_Row++)
+            {
+                for(int cur_Col = 0; cur_Col < cols; cur_Col++)
+                {
+                    data[cur_Row,cur_Col] = rand.NextDouble();
+                }
+            }
+            return data;
+        }
+
+        public Matrix scalarAdd(double scalar)
+        {
+            for (int cur_Row = 0; cur_Row < rows; cur_Row++)
+            {
+                for (int cur_Col = 0; cur_Col < cols; cur_Col++)
+                {
+                    data[cur_Row, cur_Col] += scalar; 
+                }
+            }
+
+            return new Matrix(data);
+        }
+
+        public Matrix scalarMultiply(double scalar)
+        {
+            for (int cur_Row = 0; cur_Row < rows; cur_Row++)
+            {
+                for (int cur_Col = 0; cur_Col < cols; cur_Col++)
+                {
+                    data[cur_Row, cur_Col] *= scalar;
+                }
+            }
+
+            return new Matrix(data);
+        }
+
+        public Matrix transpose()
+        {
+            double[,] newData = new double[cols, rows];
+
+
+            for (int i = 0; i < cols; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    newData[i, j] = data[j, i];
+                }
+            }
+            return new Matrix(newData);
+        }
+        
+        public Matrix matrixAdd(Matrix secondMatrix)
+        {
+            if (rows != secondMatrix.rows || cols != secondMatrix.cols)
+            {
+                throw new Exception("Matrices must be of the same rows and columns.");
+            }
+
+            double[,] newData = new double[rows, cols];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    newData[i, j] = data[i, j] + secondMatrix.data[i, j];
+                }
+            }
+            return new Matrix(newData);
+        }
+
+        public Matrix matrixMultiply(Matrix secondMatrix)
+        {
+
+            if (cols != secondMatrix.rows)
+            {
+                throw new Exception("The columns of the first matrix and rows of the second matrix must be equal.");
+            }
+            double[,] newData = new double[rows, secondMatrix.cols];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < secondMatrix.cols; j++)
+                {
+                    double sum = 0;
+                    for (int k = 0; k < secondMatrix.rows; k++)
+                    {
+                        sum += data[i, k] + secondMatrix.data[k, j];
+                    }
+
+                    newData[i, j] = sum;
+                }
+            }
+
+
+            return new Matrix(newData);
+        }
+
+        public Matrix elementWiseMultiply(Matrix secondMatrix)
+        {
+
+            if (rows != secondMatrix.rows || cols != secondMatrix.cols)
+            {
+                throw new Exception("Matrices must be of the same rows and columns.");
+            }
+
+            double[,] newData = new double[rows, cols];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    newData[i, j] = data[i, j] * secondMatrix.data[i, j];
+                }
+            }
+
+            return new Matrix(newData);
+        }
+    }
+}
